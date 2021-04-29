42d28961757aaaddf4c7dc7cdb8e9c9cfc86f383
Michael Hagar <michael.hagar@schooladmin.com>
2021-04-28 21:04:27 -0500

    add authenticated password specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe User, type: :model do
 
     expect(u).to_not be_valid
   end
+
+  it 'authenticates with valid password' do
+    u = User.create(email: 'test@email.com', username: 'test_user', fullname: 'test_name', password: 'test_password')
+    u.save!
+    u = User.find(u.id)
+
+    expect(u.authenticate('invalid_pass')).to eq(false)
+    authenticated_user = u.authenticate('test_password')
+    expect(authenticated_user.email).to eq('test@email.com')
+  end
 end
