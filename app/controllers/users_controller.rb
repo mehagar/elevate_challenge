5d722a24068d9d42fddf30552ed4a73cd0293fd1
Michael Hagar <michael.hagar@schooladmin.com>
2021-04-28 21:44:47 -0500

    test signup of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def sign_up
+    User.create(user_params)
+
+    head :created
+  end
+
+  def user_params
+    params.permit(:email, :username, :fullname, :password)
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,9 +4,11 @@ RSpec.describe 'UsersController', type: :request do
   let(:headers) { { 'CONTENT_TYPE' => 'application/json' } }
 
   it 'allows signing up' do
-    json_params = { email: 'test_email', username: 'test_user', full_name: 'first last', password: 'testpassword' }.to_json
+    json_params = { email: 'test_email', username: 'test_user', fullname: 'first last', password: 'testpassword' }.to_json
+
     post '/api/users/sign_up', params: json_params, headers: headers
 
     expect(response).to have_http_status(:created)
+    expect(User.find_by(email: 'test_email')).to_not be_nil
   end
 end
