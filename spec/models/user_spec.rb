19131b4efca1f42c6123f017b317d3795627c445
Michael Hagar <michael.hagar@schooladmin.com>
2021-04-28 20:53:07 -0500

    add initial user spec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'works' do
-    expect(1).to eq(1)
+  it 'validates required fields' do
+    u = User.new(email: nil, username: nil, fullname: nil, password: nil)
+
+    expect(u).to_not be_valid
   end
 end
